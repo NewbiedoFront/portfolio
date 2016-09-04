909db71167efe15df9ece4209efc1ee3da644a7c
Iury Chagas <iurychagas@icloud.com>
2016-09-04 06:46:07 -0300

    commit all the files 💀

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+document.querySelector('.abrir-menu').onclick = function(){
+	document.documentElement.classList.add('menu-ativo');
+};
+document.documentElement.onclick = function(event){
+	if (event.target === document.documentElement){
+		document.documentElement.classList.remove('menu-ativo');
+	}
+}
